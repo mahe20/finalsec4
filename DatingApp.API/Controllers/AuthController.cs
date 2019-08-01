7af82b6956d3016e2ed5b92e4ae431d9341b7c9a
Mahe20 <mahendraadhikari2012@gmail.com>
2019-08-01 16:20:21 -0400

    upto50

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -50,6 +50,8 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
+              throw new Exception("PC says no");
+
             var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
 
             if (userFromRepo == null)
@@ -80,6 +82,9 @@ namespace DatingApp.API.Controllers
             return Ok (new {
                 token = tokenHandler.WriteToken(token)
             });
+
+            
+        
         }
 
     }
